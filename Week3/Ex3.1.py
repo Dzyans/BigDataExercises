5b99b292bcfa66d0ae30fcfa8e5c6cbb433a0bd6
emilhvid <s123725@student.dtu.dk>
2017-09-21 18:37:33 +0200

    first exercise

---
+++ Week3/Ex3.1.py
@@ -0,0 +1,10 @@
+import numpy
+
+f = open ( 'matrixFile.txt' , 'r')
+l = [ map(int,line.split(',')) for line in f ]
+
+a = numpy.array([[l[0][0],l[1][0],l[2][0]], [l[0][1],l[1][1],l[2][1]],[l[0][2],l[1][2],l[2][2]]])
+b = numpy.array([l[0][3],l[1][3],l[2][3]])
+x = numpy.linalg.solve(a, b)
+
+print x
