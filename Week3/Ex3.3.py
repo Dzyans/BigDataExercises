65ee62cf7cc0d4802943cf0c8f49313e734506ec
emilhvid <s123725@student.dtu.dk>
2017-09-21 21:02:56 +0200

    excersice 3 solved

--- Week3/Ex3.3.py
+++ Week3/Ex3.3.py
@@ -1,3 +1,17 @@
+import timeit
+
+start = timeit.default_timer()
+print (sumFunction(1,10000))
+stop = timeit.default_timer()
+
+print stop - start 
+
+def sumFunction(n,m):
+    result = 0
+    for n in range (n,m):
+        result = result + 1/float(n*n)
+    return result        
+
 def new_method():
     print("heasdfasdfsafsafj")
 
