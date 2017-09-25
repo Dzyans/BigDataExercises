f3a4e53ae57becbaca3daa6a693bb62430aa8c70
emilhvid <s123725@student.dtu.dk>
2017-09-25 09:07:43 +0200

    cleaned exercise 1

--- Week3/Ex3.1.py
+++ Week3/Ex3.1.py
@@ -1,10 +1,11 @@
-import numpy
+import numpy as np
 
 f = open ( 'matrixFile.txt' , 'r')
-l = [ map(int,line.split(',')) for line in f ]
 
-a = numpy.array([[l[0][0],l[1][0],l[2][0]], [l[0][1],l[1][1],l[2][1]],[l[0][2],l[1][2],l[2][2]]])
-b = numpy.array([l[0][3],l[1][3],l[2][3]])
-x = numpy.linalg.solve(a, b)
+with open('matrixFile.txt') as file:
+    array2d = [[int(digit) for digit in line.split(',')] for line in file]
+
+A = np.asmatrix(array2d)
+x = np.linalg.solve((A[:,0:3]), (A[:,3]))
 
 print (x)
