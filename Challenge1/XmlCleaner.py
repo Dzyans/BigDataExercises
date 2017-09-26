cbced4bab3570698993b97dcf39e17c30e11e22e
Dzyans <SatansYngels22>
2017-09-26 14:19:11 +0200

    unzipping ftw

--- Challenge1/XmlCleaner.py
+++ Challenge1/XmlCleaner.py
@@ -1,8 +1,14 @@
 import zipfile as zip
 import os
 def iterate_dir(path):
-    dir = path
+    dir = os.path.abspath("")
     extension = ".zip"
     for item in os.listdir(dir):
         if item.endswith(extension):
-            zip.ZipFile.extract(item)
+            zip_file = zip.ZipFile(item, 'r')
+            print(os.path.join(dir, item))
+            zip_file.extractall(dir+"/out")
+            zip_file.close()
+
+
+iterate_dir("jek")
