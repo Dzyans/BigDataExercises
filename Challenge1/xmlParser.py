f577e36efad5e215d5312cb32a861459a593f855
dzyan <s144867@student.dtu.dk>
2017-10-03 13:42:41 +0200

    first iteration, prints acceptable duncan

--- Challenge1/xmlParser.py
+++ Challenge1/xmlParser.py
@@ -1,45 +1,30 @@
 
 import xml.etree.ElementTree as etree
-def parseAndWrite():
+def parseAndWrite(path, id_tag):
     counter = 0
-    with open("/home/dzyan/Dokumenter/enwiki-20170820-pages-articles-multistream.xml") as xml:
+    ##"{http://www.mediawiki.org/xml/export-0.10/}"
+    #with open("/home/dzyan/Dokumenter/enwiki-20170820-pages-articles-multistream.xml") as xml:
+    with open(path) as xml:
         for event, elem in etree.iterparse(xml, events=('start', 'end', 'start-ns', 'end-ns')):
-            if event == "end" and elem.tag == "{http://www.mediawiki.org/xml/export-0.10/}page":
+            if event == "end" and elem.tag == id_tag+"page":
                 for a in elem:
-                    if a.tag == "{http://www.mediawiki.org/xml/export-0.10/}revision":
+                    if a.tag == id_tag+"title":
+                        print a.text
+                        if a.text[0] != "H" and a.text[0] != "h":
+                            print a.text
+                            print "-----------------------break-----------------------"
+                            return
+                    if a.tag == id_tag+"revision":
                         for b in a:
-                            if b.tag == "{http://www.mediawiki.org/xml/export-0.10/}text":
-                                print b.text
+                            if b.tag == id_tag+"text":
+                                print cleanText(b.text)
         elem.clear()
 
                 ##print "found"
     print counter
 
 
-def anohterParser():
-    with open("/home/dzyan/Dokumenter/enwiki-20170820-pages-articles-multistream.xml") as xml:
+def cleanText(text):
+    return text.replace('\n', ' ')
 
-        # get an iterable
-        context = etree.iterparse(xml, events=("start", "end"))
-
-        # turn it into an iterator
-        context = iter(context)
-
-        # get the root element
-        event, root = context.next()
-
-        for event, elem in context:
-            if event == "end" and elem.tag == "{http://www.mediawiki.org/xml/export-0.10/}page":
-
-
-
-                root.clear()
-def pp():
-    import xml.etree.ElementTree as ET
-    tree = ET.parse('/home/dzyan/Dokumenter/enwiki-20170820-pages-articles-multistream.xml')
-
-
-
-#pp()
-parseAndWrite()
-#anohterParser()
+parseAndWrite("")
