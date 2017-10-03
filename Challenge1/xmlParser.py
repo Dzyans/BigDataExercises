de730943a7a6b69a394d444c6ad86f136119bb14
dzyan <s144867@student.dtu.dk>
2017-10-03 15:08:53 +0200

    can now write articles starting with a to dir

--- Challenge1/xmlParser.py
+++ Challenge1/xmlParser.py
@@ -2,29 +2,49 @@
 import xml.etree.ElementTree as etree
 def parseAndWrite(path, id_tag):
     counter = 0
+    metaCounter = 0
+    string_list = []
     ##"{http://www.mediawiki.org/xml/export-0.10/}"
     #with open("/home/dzyan/Dokumenter/enwiki-20170820-pages-articles-multistream.xml") as xml:
     with open(path) as xml:
         for event, elem in etree.iterparse(xml, events=('start', 'end', 'start-ns', 'end-ns')):
             if event == "end" and elem.tag == id_tag+"page":
+                proceed = False
                 for a in elem:
                     if a.tag == id_tag+"title":
-                        print a.text
-                        if a.text[0] != "H" and a.text[0] != "h":
-                            print a.text
-                            print "-----------------------break-----------------------"
-                            return
-                    if a.tag == id_tag+"revision":
-                        for b in a:
-                            if b.tag == id_tag+"text":
-                                print cleanText(b.text)
-        elem.clear()
-
-                ##print "found"
-    print counter
+                        #print a.text
+                        if a.text[0] == "A" or a.text[0] == "a":
+                            proceed = True
+                            counter = counter +1
+                            metaCounter = metaCounter+1
+                            #print counter
+                        #if(metaCounter > 1000):
+                         #   print "-----------done for now, "+ str(counter) +" files written"
+                          #  return string_list
+                    if a.tag == id_tag+"revision" and proceed:
+                       for b in a:
+                          if b.tag == id_tag+"text":
+                             string_list.append(cleanText(b.text) + u"\n" + u"\n")
+                if len(string_list) == 100:
+                    writeToFile(string_list, "A/A"+ str(metaCounter/100))
+                    ##reset and go on
+                    counter = 0
+                    string_list = []
+                elem.clear()
 
 
 def cleanText(text):
     return text.replace('\n', ' ')
 
-parseAndWrite("")
+def writeToFile(the_list, filepath):
+    print "writing " + str(len(the_list)) + " to " + filepath
+    with open(filepath, "w+") as file_handler:
+        for item in the_list:
+            file_handler.write(item.encode('utf8'))
+
+
+path = "/home/dzyan/Dokumenter/enwiki-20170820-pages-articles-multistream.xml"
+tag_id = "{http://www.mediawiki.org/xml/export-0.10/}"
+listen = parseAndWrite(path, tag_id)
+#print listen
+writeToFile(listen, "A")
