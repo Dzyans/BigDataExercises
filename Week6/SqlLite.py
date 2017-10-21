383622a930f1cbe8e3747ccde3d750290955d5d8
dzyan <s144867@student.dtu.dk>
2017-10-21 15:29:28 +0200

    exercise 3 done

--- Week6/SqlLite.py
+++ Week6/SqlLite.py
@@ -64,6 +64,22 @@ def db_shell():
 ##Select * from (Select * From Orders) OO INNER JOIN (Select * from (select * from 'Order Details' where OrderID IN(Select OrderID from Orders where OrderID in (Select OrderId from (Select OrderID, Count(OrderID) as oc from 'Order Details' where OrderID in (Select OrderID from Orders where CustomerID = 'ALFKI') group by OrderID) where oc > 1))) OD INNER JOIN (select * from Products) P on OD.ProductID = P.ProductID) ODD on OO.OrderID = ODD.OrderID;
 ## trimmed a bit
 ## ##Select * from (Select OrderID, CustomerID From Orders) OO INNER JOIN (Select * from (select OrderID, ProductID, Quantity from 'Order Details' where OrderID IN(Select OrderID from Orders where OrderID in (Select OrderId from (Select OrderID, Count(OrderID) as oc from 'Order Details' where OrderID in (Select OrderID from Orders where CustomerID = 'ALFKI') group by OrderID) where oc > 1))) OD INNER JOIN (select ProductID, ProductName from Products) P on OD.ProductID = P.ProductID) ODD on OO.OrderID = ODD.OrderID;
+
+#Excer 3
+#Create VIEW totals AS
+#Select * FROM
+#  (SELECT ProductName,
+#          ProductID
+#   FROM Products) PRODS
+#INNER JOIN
+#  (SELECT ProductID,
+#          Sum(Quantity) AS Total
+#   FROM 'Order Details'
+#   WHERE OrderID IN
+#       (SELECT OrderID
+#        FROM Orders
+#        WHERE CustomerID = 'ALFKI')
+#   GROUP BY ProductID) COUNTS ON PRODS.ProductID = Counts.ProductID;
 GO()
 
 db_shell()
