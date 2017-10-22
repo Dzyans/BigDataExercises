690c6fd13850c13fe3dc8f36b11299e27ec61d7e
emilhvid <hvid2301@gmail.com>
2017-10-22 15:05:52 +0200

    tror jeg har fat i de rigtige

--- Week6/MongoDB.py
+++ Week6/MongoDB.py
@@ -10,32 +10,50 @@ from pymongo import MongoClient as Mongo
 client = Mongo('localhost',27017)
 
 db = client['Northwind']
-print client.database_names()
 
+import pprint
 
-print db.collection_names()
+#pprint.pprint(list(db.orders.aggregate([
+#        {'$match' : { 'CustomerID' : 'ALFKI' }}])
+#))
 
-orders  = db.orders
-order_details = db["order-details"]
-products = db.products
-customers = db.customers
-
-#print customers.find_one({"CustomerID ": "ALFKI"})
-
-#print orders.find_one({ "CustomerID" : 'ALFKI' })
-
-
+pprint.pprint(list(db.orders.aggregate([
+        {'$lookup': {
+        'from': 'order-details', 
+        'localField': 'OrderID', 
+        'foreignField': 'OrderID', 
+        'as': 'order_details'
+            }
+        },
+        {'$unwind': '$order_details'},
+        {'$lookup': {'from' : 'products',
+                     'localField': "ProductID",
+                     'foreignField': 'order_details.ProductID',
+                     'as': 'product_details'
+         }},
+        {'$unwind': '$product_details'},
+        {'$match' : { 'CustomerID' : 'ALFKI' }}      
+    ])
+))
 
 #Exercise 1
+orders  = db.orders
+#order_details = db["order-details"]
+#products = db.products
+#customers = db.customers
 
-for order in orders.find({ "CustomerID" : "ALFKI" }):
- #   #print order["OrderID"]
-    for detail in order_details.find({"OrderID" : order["OrderID"]}):
-   #     print detail["ProductID"]
-        for product in products.find({"ProductID" : detail["ProductID"]}):
-            print product["ProductName"]
+
+#for order in orders.find({ "CustomerID" : "ALFKI" }):
+#    print order.order_details
+#    for detail in order_details.find({"OrderID" : order["OrderID"]}):
+#        print detail["ProductID"]
+#        for product in products.find({"ProductID" : detail["ProductID"]}):
+#            product["ProductName"]
 
 
 # Exercise 2
             
-
+#for order in orders.find({ "CustomerID" : "ALFKI" }):
+    #print order["OrderID"]
+ #   for detail in order_details.find({"OrderID" : order["OrderID"]}):
+#        detail
