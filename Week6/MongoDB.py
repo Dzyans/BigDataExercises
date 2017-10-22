071b288fdaa24ea74008464bfa1b95b02933d73e
emilhvid <hvid2301@gmail.com>
2017-10-22 19:43:27 +0200

    opg2 done

--- Week6/MongoDB.py
+++ Week6/MongoDB.py
@@ -16,8 +16,8 @@ import pprint
 # Exercise 1
 #db.orders.aggregate([
 logFile=open('mylogfile'+'.txt', 'w')
-
-pprint.pprint(list(db.orders.aggregate([
+#pprint.pprint(list(db.orders.aggregate([
+db.orders.aggregate([
         {'$lookup': {
         'from': 'order-details', 
         'localField': 'OrderID', 
@@ -36,17 +36,54 @@ pprint.pprint(list(db.orders.aggregate([
         {'$project':{
             'CustomerID': 1,
             'OrderID': 1,
-            'order_details' : '$order_details.ProductID',
-            'product_details' : '$product_details.ProductName'
+            'order_details' : '$order_details',
+            'product_details' : '$product_details'
         }},
         {'$group' : {
             '_id' : {'OrderID' : "$OrderID"},
             'details': { '$push': '$$ROOT'},
             'count' : { '$sum' : 1 }}}
     ])
-), logFile)
-
+#), logFile)
 
 logFile.close()
+
 # Exercise 2
-            
+logFile=open('Exercise_2'+'.txt', 'w')
+pprint.pprint(list(db.orders.aggregate([
+#db.orders.aggregate([
+        {'$lookup': {
+        'from': 'order-details', 
+        'localField': 'OrderID', 
+        'foreignField': 'OrderID', 
+        'as': 'order_details'
+            }},
+        {'$lookup': {'from' : 'products',
+             'localField': "order_details.ProductID",
+             'foreignField':'ProductID',
+             'as': 'product_details'
+         }},
+        {'$unwind': '$order_details'},
+        # define some conditions here 
+        {'$match' : { 'CustomerID' : 'ALFKI' }},
+        #define which fields are you want to fetch
+        {'$project':{
+            'CustomerID': 1,
+            'OrderID': 1,
+            'order_details' : '$order_details',
+            'product_details' : '$product_details' ,
+            'size_of_products': {'$size': "$product_details"}
+
+        }},
+        {'$match': {"size_of_products": {'$gt': 1}}},
+        {'$group' : {
+            '_id' : {'OrderID' : "$OrderID"},
+            'details': { '$push': '$$ROOT'},
+            'count' : { '$sum' : 1 }
+            }}
+        
+    ])
+), logFile)            
+
+# Exercise 3
+    
