6e0188254bb6803c76d41e70814bda7846c20261
dzyan <s144867@student.dtu.dk>
2017-10-24 10:41:31 +0200

    minor cleanup, added queri text for ex 2

--- Week6/SqlLite.py
+++ Week6/SqlLite.py
@@ -8,18 +8,8 @@ def GO():
     # Get all tables
     c.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;")
 
-
-
     print c.fetchall()
 
-    # Insert a row of data
-    #c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
-
-    # Save (commit) the changes
-    #conn.commit()
-
-    # We can also close the connection if we are done with it.
-    # Just be sure any changes have been committed or they will be lost.
     conn.close()
 
 def db_shell():
@@ -64,6 +54,8 @@ def db_shell():
 ##Select * from (Select * From Orders) OO INNER JOIN (Select * from (select * from 'Order Details' where OrderID IN(Select OrderID from Orders where OrderID in (Select OrderId from (Select OrderID, Count(OrderID) as oc from 'Order Details' where OrderID in (Select OrderID from Orders where CustomerID = 'ALFKI') group by OrderID) where oc > 1))) OD INNER JOIN (select * from Products) P on OD.ProductID = P.ProductID) ODD on OO.OrderID = ODD.OrderID;
 ## trimmed a bit
 ## ##Select * from (Select OrderID, CustomerID From Orders) OO INNER JOIN (Select * from (select OrderID, ProductID, Quantity from 'Order Details' where OrderID IN(Select OrderID from Orders where OrderID in (Select OrderId from (Select OrderID, Count(OrderID) as oc from 'Order Details' where OrderID in (Select OrderID from Orders where CustomerID = 'ALFKI') group by OrderID) where oc > 1))) OD INNER JOIN (select ProductID, ProductName from Products) P on OD.ProductID = P.ProductID) ODD on OO.OrderID = ODD.OrderID;
+
+
 
 #Excer 3
 #Create VIEW totals AS
