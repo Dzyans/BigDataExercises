428d87c82a5404965e77139f5ad8ac8face3fdd4
emilhvid <hvid2301@gmail.com>
2017-10-30 13:14:12 +0100

    solved exercise 2 i think semi

---
+++ Week7/EulerGraphs.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 27 16:29:59 2017
+
+@author: s123725
+"""
+
+import sys
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import re
+
+WORD_RE = re.compile(r"[\w']+")
+
+
+class eulerGraphs(MRJob):
+    def steps(self):
+        return [MRStep(mapper_init=self.splitFile,
+                       mapper=self.get_edges,
+                       mapper_final=self.final_get_edges,
+                       combiner=self.sum_edges,
+                       reducer=self.even_edges,
+                       )]
+    
+    def splitFile(self):
+        self.edges = {}
+        
+    def get_edges(self, _, line):
+        for edge in WORD_RE.findall(line):
+            self.edges.setdefault(edge, 0)
+            self.edges[edge] = self.edges[edge] + 1
+
+    def final_get_edges(self):
+        for edge, val in self.edges.iteritems():
+            yield edge, val
+
+    def sum_edges(self, edge, counts):
+        yield edge, sum(counts)    
+    
+    def even_edges(self, edge, counts):
+        yield edge, sum(counts) % 2 == 0    
+    
+if __name__ == '__main__':
+    eulerGraphs.run()
--- Week7/countOccurrencesMrJob.py
+++ Week7/countOccurrencesMrJob.py
@@ -12,15 +12,13 @@ import re
 WORD_RE = re.compile(r"[\w']+")
 
 class OccurencesCount(MRJob):
-    """
-    def mapper(self,  _, line):
-        yield 'chars', len(line)
-        yield 'words', len(line.split())
-        yield 'lines', 1
-    
-    def reducer(self, key, values):
-        yield key, sum(values)
-    """
+    def steps(self):
+        return [MRStep(mapper_init=self.init_get_words,
+                       mapper=self.get_words,
+                       mapper_final=self.final_get_words,
+                       combiner=self.sum_words,
+                       reducer=self.sum_words)]
+
     def init_get_words(self):
         self.words = {}
 
@@ -37,17 +35,9 @@ class OccurencesCount(MRJob):
     def sum_words(self, word, counts):
         yield word, sum(counts)
 
-    def steps(self):
-        return [MRStep(mapper_init=self.init_get_words,
-                       mapper=self.get_words,
-                       mapper_final=self.final_get_words,
-                       combiner=self.sum_words,
-                       reducer=self.sum_words)]
 
 if __name__ == '__main__':
     if (len(sys.argv) != 2):
         print "Program requires path to file for reading!"
         sys.exit(1)
     OccurencesCount.run()
-
-#/home/dzyan/PycharmProjects/BigDataExercises/Challenge1/A/part1
