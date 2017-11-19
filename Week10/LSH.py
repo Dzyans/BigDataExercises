60407c8606140893e2c392fce85f570f7cbb6ff5
emilhvid <hvid2301@gmail.com>
2017-11-19 23:50:19 +0100

    lsh in function, added example two

--- Week10/LSH.py
+++ Week10/LSH.py
@@ -2,21 +2,21 @@ from PIL import Image
 import numpy as np
 
 
+def LSH(image1, image2):
+    pix_image1 = image1.resize((8,9),Image.ANTIALIAS)
+    pix_image2 = image2.resize((8,9),Image.ANTIALIAS)
+    
+    list_image1 = list(pix_image1.getdata())
+    list_image2 = list(pix_image2.getdata())
+    
+    lsh = [i > j for i,j in zip(list_image1,list_image2)]
+    
+    return lsh
+
 cat_one = Image.open('Cat1.png').convert('LA')
 cat_two = Image.open('Cat2.png').convert('LA')
 
-pix_cat_one = cat_one.resize((8,9),Image.ANTIALIAS)
-pix_cat_two = cat_two.resize((8,9),Image.ANTIALIAS)
+print LSH(cat_one,cat_two)
 
-list_cat_one = list(pix_cat_one.getdata())
-list_cat_two = list(pix_cat_two.getdata())
-
-z = [i > j for i,j in zip(list_cat_one,list_cat_two)]
-
-print z
-#pix_cat_one.show()
-
-#img = mpimg.imread('image.png')     
-#gray = rgb2gray(img)    
-#plt.imshow(gray, cmap = plt.get_cmap('gray'))
-#plt.show()
+dog_one = Image.open('dog1.jpg').convert('LA')
+print LSH(cat_one,dog_one)
