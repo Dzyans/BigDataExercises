a0b1ac117729529a136e79046fa6bb495f1e6ef1
emilhvid <hvid2301@gmail.com>
2017-11-19 20:46:20 +0100

    laver en matrix over bag of words

--- Week10/word_bagger.py
+++ Week10/word_bagger.py
@@ -7,11 +7,15 @@ Created on Sat Nov 18 23:05:34 2017
 import json
 import pandas as pd
 import os
+import collections, re
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy
 
 def bag_the_words():
     #print("doing stuff")
     # pd.json.loads("C:\\Users\\tadah\\Documents\\BigDataExercises\\Week10\\reuters-010.json")
     c = 0
+    texts = []
     for filename in os.listdir('.\\'):
         if filename.endswith(".json"): 
             # print(os.path.join(directory, filename))
@@ -25,10 +29,25 @@ def bag_the_words():
                     elif 'topics' not in data:
                         data.pop
                     else:
+                        texts.append((data['body']).lower())
                         c+=1
             continue
         else:
             continue
     print c
-   
+    #create_bag_of_words(texts)
+    verctorize_bag_of_words(texts)
+
+def create_bag_of_words(texts):
+    bagsofwords = [ collections.Counter(re.findall(r'\w+', txt)) for txt in texts]
+    print bagsofwords[0]
+
+def verctorize_bag_of_words(texts):
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(texts) 
+    array = X.toarray()
+    print numpy.shape(array)
+    featurenames = vectorizer.get_feature_names()
+
+
 bag_the_words()
