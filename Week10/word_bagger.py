7347e525df9713eb5d283c9523283ed4cb5ef0a0
Thor <Thor@roger.ai>
2017-11-19 21:52:24 +0100

    little file iteration fix

--- Week10/word_bagger.py
+++ Week10/word_bagger.py
@@ -13,11 +13,19 @@ import numpy
 
 def bag_the_words():
     c = 0
+    file_counter = 0
     texts = []
     for filename in os.listdir('.\\'):
+        file_nr = ""
+        print (file_counter)
         if filename.endswith(".json"): 
             # print(os.path.join(directory, filename))
-            with open('reuters-010.json') as data_file:    
+            if file_counter < 10:
+                file_nr = "00" + str(file_counter)
+            else:
+                file_nr = "0" + str(file_counter)
+            with open('reuters-'+ file_nr+'.json') as data_file:    
+                print('reuters-'+ file_nr+'.json')
                 json_object = json.load(data_file)
                 #print(data[4]["title"])
                 for data in json_object:
@@ -32,23 +40,24 @@ def bag_the_words():
                         body = re.sub(' +',' ',body)
                         texts.append(body)
                         c+=1
+            file_counter += 1
             continue
         else:
             continue
-    print c
+    print (c)
     #create_bag_of_words(texts)
     verctorize_bag_of_words(texts)
 
 def create_bag_of_words(texts):
     bagsofwords = [ collections.Counter(re.findall(r'\w+', txt)) for txt in texts]
     sumbags = sum(bagsofwords, collections.Counter())
-    print len(sumbags)
+    print (len(sumbags))
 
 def verctorize_bag_of_words(texts):
     vectorizer = CountVectorizer()
     X = vectorizer.fit_transform(texts) 
     array = X.toarray()
-    print numpy.shape(array)
+    print (numpy.shape(array))
     featurenames = vectorizer.get_feature_names()
     #print featurenames
 bag_the_words()
